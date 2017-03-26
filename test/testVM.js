6adcaabd0badb748eeec3a0606b554d405534a75
danigb <danigb@gmail.com>
2017-03-27 00:56:19 +0200

    More tests

--- test/testVM.js
+++ test/testVM.js
@@ -1,15 +1,17 @@
 import { VM } from "../src/vm";
 
 // Setup a VM for testing
-export default function initVM () {
+export default function initVM (ext) {
   const vm = new VM()
   vm.printed = [];
+  if (ext) vm.addCommands(ext)
   vm.addCommands({
-    "@print": ({ stack }) => vm.printed.push(stack.pop())
+    "@print": ({ stack }) => vm.printed.push(stack.pop()),
+    "@ptime": (proc) => vm.printed.push(proc.time.toFixed(2))
   });
-  vm.run = prog => {
+  vm.run = (prog, dur = Infinity) => {
     vm.fork(null, null, prog);
-    vm.resume(Infinity);
+    if (dur) vm.resume(dur);
   };
   return vm;
 }
